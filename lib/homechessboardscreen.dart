b258a4ed177adc88aff448faf4b0f02775143319
Joe Black <oniyajoeolaiya@gmail.com>
2021-09-18 12:48:18 +0100

    Nice chessApp

---
+++ lib/homechessboardscreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeChessBoardScreen extends StatelessWidget {
+  const HomeChessBoardScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
